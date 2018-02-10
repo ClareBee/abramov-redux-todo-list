e4f857373d270d7d5c1811cb849f6252ad82312f
Clare <chblackburne@gmail.com>
2018-02-10 18:25:49 +0000

    Refactors to use combineReducer method

---
+++ src/Reducers/index.js
@@ -0,0 +1,24 @@
+
+import { combineReducers } from 'redux';
+import todos from './AddToDoReducer';
+import visibilityFilter from './SetVsibilityReducer';
+// const todoApp = (state = {}, action) => {
+//   return (
+//     todos: todos(
+//       state.todos,
+//       action
+//     ),
+//     visibilityFilter: visibilityFilter(
+//       state.visibilityFilter,
+//       action
+//     )
+//   );
+// };
+
+//top level reducer which maps state name and reducers
+//keys correspond to fields of state object
+//values are reducers that are called to update corresponding state fields
+export const todoApp = combineReducers({
+  todos,
+  visibilityFilter
+})
--- src/index.js
+++ src/index.js
@@ -2,7 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { createStore } from 'redux';
 import registerServiceWorker from './registerServiceWorker';
+import todoApp from './Reducers';
+
+const store = createStore(todoApp);
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
