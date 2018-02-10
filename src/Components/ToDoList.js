e03f6d5bd58a6c1cf62fee5a12c455baa331349d
Clare <chblackburne@gmail.com>
2018-02-10 17:53:22 +0000

    Adds basic view for list

---
+++ src/Components/ToDoList.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class ToDoList extends React.Component {
+  render(){
+    return(
+      <React.Fragment>
+        <h1>todolist</h1>
+      </React.Fragment>
+    )
+  }
+}
+
+export default ToDoList;
